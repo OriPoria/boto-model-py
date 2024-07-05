98f8f34a37212399c6210862d4c3ec33b5147349
ori <ori_poria_tp@bmc.com>
2024-07-05 23:09:57 +0300

    edit tests

--- tests/integration_tests/test.py
+++ tests/integration_tests/test.py
@@ -1,13 +1,10 @@
 import boto3
-from list_buckets_output import ListBucketsResponse
+from dist.list_buckets_response import ListBucketsResponse
+
 
 service = "s3"
-bucket_name = "jira-tickets-dba"
 client = boto3.client(service, region_name="us-east-2")
-# response = client.list_bucket_analytics_configurations(Bucket=bucket_name)
-# buckets_model = IsTruncated(**response)
 buckets = client.list_buckets()
-buckets_model = ListBucketsResponse(**buckets)
-print(buckets_model.Buckets[0].Name)
-print(buckets_model)
-# buckets_model.Buckets[1].CreationDate
+buckets_object = ListBucketsResponse(**buckets)
+for buck in buckets_object.Buckets:
+    print(buck.Name)
